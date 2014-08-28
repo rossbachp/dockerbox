e54f845602d226f2694848eb0df55dcc98ffd5d6
Peter Rossbach <peterrossbach@me.com>
2014-08-28 13:29:08 +0200

    optimize docker images size: now 570mb

--- docker-images/tomcat8/tomcat.sh
+++ docker-images/tomcat8/tomcat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ ! -f /.tomcat_user_created ]; then
-    /create_tomcat_user.sh
+if [ ! -f ${CATALINA_HOME}/.tomcat_created ]; then
+  ${CATALINA_HOME}/bin/create_tomcat_user.sh
 fi
 
 DIR=${DEPLOY_DIR:-/webapps}
@@ -28,6 +28,8 @@ fi
 #-XX:OnError="cmd args; cmd args"
 #-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp/heapdump.hprof -XX:OnOutOfMemoryError="sh ~/cleanup.sh"
 
+export LANG="en_US.UTF-8"
+export JAVA_OPTS="$JAVA_OPTS -Duser.language=en -Duser.country=US"
 export CATALINA_PID=${CATALINA_HOME}/temp/tomcat.pid
 export CATALINA_OPTS="$CATALINA_OPTS  -Xmx${JAVA_MAXMEMORY}m -DjvmRoute=${TOMCAT_JVM_ROUTE}  -Dtomcat.maxThreads=${TOMCAT_MAXTHREADS}  -Dtomcat.minSpareThreads=${TOMCAT_MINSPARETHREADS}  -Dtomcat.httpTimeout=${TOMCAT_HTTPTIMEOUT}  -Djava.security.egd=file:/dev/./urandom"
 exec ${CATALINA_HOME}/bin/catalina.sh run
